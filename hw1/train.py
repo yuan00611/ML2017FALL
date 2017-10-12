c7b3f3e2380d100f7896d009f48a073c8a481acf
陳德元 <yuan0061@gmail.com>
2017-10-12 10:19:17 +0800

    upload hw1 simple baseline

---
+++ hw1/train.py
@@ -0,0 +1,101 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import csv 
+import numpy as np
+from numpy.linalg import inv
+import random
+import math
+import sys
+
+
+# In[2]:
+
+data = []
+# 每一個維度儲存一種污染物的資訊
+for i in range(18):
+	data.append([])
+
+n_row = 0
+text = open('train.csv', 'r', encoding='big5') 
+row = csv.reader(text , delimiter=",")
+for r in row:
+    # 第0列沒有資訊
+    if n_row != 0:
+        # 每一列只有第3-27格有值(1天內24小時的數值)
+        for i in range(3,27):
+            if r[i] != "NR":
+                data[(n_row-1)%18].append(float(r[i]))
+            else:
+                data[(n_row-1)%18].append(float(0))	
+    n_row = n_row+1
+text.close()
+
+
+# In[3]:
+
+x = []
+y = []
+# 每 12 個月
+for i in range(12):
+    # 一個月取連續10小時的data可以有471筆
+    for j in range(471):
+        x.append([])
+        
+        # 18種污染物
+        for t in range(18):
+            # 連續9小時
+            for s in range(9):
+                x[471*i+j].append(data[t][480*i+j+s] )
+        y.append(data[9][480*i+j+9])  
+x = np.array(x)
+y = np.array(y)
+print (x)
+print(x.shape)
+# add square term
+x = np.concatenate((x,x**2), axis=1)
+
+# add bias
+x = np.concatenate((np.ones((x.shape[0],1)),x), axis=1)
+print(y.shape)
+
+
+# In[4]:
+
+w = np.zeros(len(x[0]))
+l_rate = 10
+repeat = 10000
+
+
+# In[9]:
+
+# use close form to check whether ur gradient descent is good
+# however, this cannot be used in hw1.sh 
+w = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)),x.transpose()),y)
+w
+
+
+# In[8]:
+
+x_t = x.transpose()
+s_gra = np.zeros(len(x[0]))
+
+for i in range(repeat):
+    hypo = np.dot(x,w)
+    loss = hypo - y
+    cost = np.sum(loss**2) / len(x)
+    cost_a  = math.sqrt(cost)
+    gra = np.dot(x_t,loss)
+    s_gra += gra**2
+    ada = np.sqrt(s_gra)
+    w = w - l_rate * gra/ada
+    print ('iteration: %d | Cost: %f  ' % ( i,cost_a))
+
+
+# In[10]:
+
+# save model
+np.save('model.npy',w)
+
